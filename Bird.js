2c6775b1bba4bfa3086934b61e256aa98ec7e1c2
amaan2004 <88380870+amaan2004@users.noreply.github.com>
2021-08-03 17:02:04 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,30 @@
+class Ball
+ {
+    constructor(x, y,width,height) {
+      var options = {
+        'density':1.5,
+        'frictionAir': 0.005,
+        
+      };
+      this.body = Bodies.rectangle(x, y, width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+      
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      strokeWeight(3);
+      stroke('blue')
+      fill('red')
+      ellipseMode(RADIUS)
+      ellipse(0, 0, this.width, this.height);
+      pop();
+    };
+  };
+  
